db3cf9413d27527f1bc3d380a923316029e2bb7c
aestes07 <aestes07@outlook.com>
2018-02-15 15:43:27 -0600

    Update app.js

--- react-backend/app.js
+++ react-backend/app.js
@@ -25,12 +25,29 @@ db.on('error', function(err) {
 var Product = require('./models/Product.js');
 
 /* GET ALL PRODUCTS */
-app.get('/products', function(req, res, next) {
-  Product.find(function (err, products) {
+// app.get('/products', function(req, res, next) {
+//  Product.find(function (err, products) {
+//    if (err) return next(err);
+//    res.json(products);
+//  });
+// });
+
+/* GET ALL Products Sorted Newest First */
+app.get('/products', (req, res, next) => {
+  Post.find({}).sort({date_posted: -1}).exec((err, products) => {
     if (err) return next(err);
     res.json(products);
   });
 });
+
+/* GET Product BY ID */
+app.get('/activeproducts', (req, res, next) => {
+  Post.findOne({_id: "5a715109131c1c8187ee4807"}, (err, products) => {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
